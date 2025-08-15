bc757195eceabc503345744296a86c190d9e1f7a
[joshua cochran] <waltwest@outlook.com>
2025-08-14 22:04:45 -0400

    adding stuff

---
+++ classes.py
@@ -0,0 +1,8 @@
+class sadiator:
+    def __init__(self, name, sex, strength):
+        self.name = name
+        self.sex = sex
+        self.health = 10
+        self.strength = strength
+        self.xp = 0
+
