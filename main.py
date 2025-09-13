94a1cf843752a9375d6f5e643ea06a0cc053c256
mi-cawber <waltwest@outlook.com>
2025-09-13 14:15:25 -0400

    added tracking for most rounds in a game and most stalemates (players have equal cards) during game

--- main.py
+++ main.py
@@ -9,6 +9,23 @@ walt = c.load_sadiator('walt')
 # returns number of rounds and stalemates (players had equal cards)
 rounds, stalemates = g.play_game(joshua, walt)
 
+# checks if game set record for highest rounds/stalemates
+with open('most_rounds.txt', 'r') as f:
+    m_rounds = f.readline()
+    m_rounds = int(m_rounds)
+
+with open('most_stalemates.txt', 'r') as f:
+    m_stalemates = f.readline()
+    m_stalemates = int(m_stalemates)
+
+if rounds > m_rounds:
+    with open('most_rounds.txt', 'w') as f:
+        f.write(str(rounds))
+
+if stalemates > m_stalemates:
+    with open('most_stalemates.txt', 'w') as f:
+        f.write(str(stalemates))
+
 # save the data
 c.save_data(joshua)
 c.save_data(walt)
